a7921ba02e8125b2a242da7830193998b43a718a
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-05-18 11:41:09 +0530

    feat:created docker container to run code

---
+++ src/containers/containerFactory.ts
@@ -0,0 +1,22 @@
+let Docker = require("dockerode");
+
+async function createContainer(imageName: string, cmdExecutable: string[]) {
+  const docker = new Docker();
+
+ const container = await docker.createContainer({
+    Image: imageName,
+    Cmd: cmdExecutable,
+
+    AttachStdin: true, //allows to send input to the container  inform of stream
+    AttachStdout: true, //allows to receive output from the container     inform of stream
+    AttachStderr: true, //allows to receive error output from the container     inform of stream
+
+    Tty: false,
+    OpenStdin: true,
+  
+  });
+
+  return container
+}
+
+export default createContainer
